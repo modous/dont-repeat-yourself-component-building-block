ba0b80ecbda037381b390ad9d6ce886db120c6d7
modous <mohamed1010@live.nl>
2023-11-25 14:09:34 +0100

    navigatie made with JS

--- src/lib/components/NavigatieBar/navigatie.svelte
+++ src/lib/components/NavigatieBar/navigatie.svelte
@@ -1,103 +1,104 @@
+<script>
+   let mobileDropdownActive = true;
+
+  function openMobileMenu() {
+    mobileDropdownActive = !mobileDropdownActive;
+  }
+</script>
+
 <section>
 	<nav class="nav-bar">
         <a href="/"><img
 			src="https://hallostrandeiland.nl/blobs/gebiedsplatform/w250h250/98691/2022/50/hseTekengebied_8.png"
 			alt="logo"
 		/></a>
-		
-		<a class="nav-link" href="/">Berichten</a>
-		<a class="nav-link" href="/">Activiteiten</a>
-		<a class="nav-link" href="/">Over Strandeiland</a>
-		<a class="nav-link" href="/">Planning</a>
-		<a class="nav-link" href="/">Participatieteam</a>
-		<a class="nav-link" href="/">Personen</a>
-		<a class="nav-link" href="/">Organisaties</a>
-		<a class="Wensen" href="/">Wensen</a>
 
-        <details>
-            <summary>
-                <div class="dropdown">
-                    <svg
-                        width="40"
-                        height="32"
-                        viewBox="0 0 44 32"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <rect width="44" height="4" fill="" />
-                        <rect y="14" width="44" height="4" fill="" />
-                        <rect y="28" width="44" height="4" fill="" />
-                    </svg>
-                    
-                </div>
-            </summary>
-            <ul>
-                <li>
-                    <a href="/">
-                        Berichten</a
-                    >
-                </li>
-                <li>
-                    <a href="/">
-                        Activiteiten</a
-                    >
-                </li>
-                <li>
-                    <a href="/">
-                        Over Strandeiland</a
-                    >
-                </li>
-                <li>
-                    <a href="/">
-                        Planning
-                </li>
-                <li>
-                    <a href="/">
-                        Participatieteam
-                </li>
-                <li>
-                    <a href="/">
-                        Personen
-                </li>
-                <li>
-                    <a href="/">
-                        Organisaties
-                </li>
-                <li>
-                    <a href="/">
-                        Wensen
-                </li>
-			</ul>
+		<ul>
+            <button class="Menu" on:click={openMobileMenu}>Menu</button>
+            {#if mobileDropdownActive}
+            <li>
+               <div> <a class="nav-link" href="/Berichten">Berichten</a></div>
+            </li>
+            <li>
+               <div> <a class="nav-link" href="/Activiteiten">Activiteiten</a></div>
+            </li>
+            <li>
+                <a class="nav-link" href="/Over Strandeiland">Over Strandeiland</a>
+            </li>
+            <li>
+                <a class="nav-link" href="/Planning">Planning</a>
+            </li>
+            <li>
+                <a class="nav-link" href="/Participatieteam">Participatieteam</a>
+            </li>
+            <li>
+                <a class="nav-link" href="/Personen">Personen</a>
+            </li>
+            <li>
+                <a class="nav-link" href="/Organisaties">Organisaties</a>
+            </li>
+            <li>
+                <a class="nav-link" href="/Wensen">Wensen</a>
+            </li>
+          {/if}
+        </ul>
+		
 	</nav>
 </section>
 
 <style>
 
+button{
+    width: 70px;
+}
+
+.Menu{
+    cursor: pointer;
+    font-weight: bold;
+    display: none;
+}
+
+
     /* Image */
 
     img{
-        max-width: 70px;
-        max-height: 70px;
+        margin: 12px;
+        width: 70px;
+        height: 70px;
+    
     }
 
 	/* Mobile nav */
-	.nav-bar {
+
+ul{
+    /* margin-left: 1px; */
+    overflow: auto;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    scroll-snap-type: x mandatory;
+    
+}
+
+li{
+    list-style-type: none;
+    }
+
+	.nav-bar {       
+        min-width: 59em;
 		display: flex;
 		background-color: var(--color-secundary-pure);
 		justify-content: space-between;
 		border-bottom-style: solid;
 		border-width: 0.5px;
-        padding-left: 50px;
-        padding-right: 50px;
+   
 	}
 
-
-   
 	.nav-link {
-		margin-top: 30px;
+        margin: 30px;
 		text-decoration: none;
 		color: black;
-        display: none;
+        display: block;
 	}
 
 	.nav-link:hover {
@@ -105,77 +106,36 @@
 		transition-duration: 1s;
 	}
 
-	.Wensen {
-		background-color: yellow;
-		text-decoration: none;
-		color: black;
-		padding: 20px;
-		border-radius: 20px;
-		margin: 10px;
-        display: none;
-	}
 
-	.Wensen:hover {
-		transform: scale(1.1);
-		transition-duration: 1s;
-	}
+	@media (max-width: 400px) {
+        .Menu{
+            display: block;
+        }
 
-	@media (min-width: 970px) {
+        ul{
+            flex-direction: column;
+            align-items: end;
+            margin: 20px;
+        }
+       
+        .nav-bar{   
+            min-width: 0px;
+        }
+
         .nav-link{
-            display: block;
+            padding-top: 10px;
+            margin: 10px;
+            
         }
-        .Wensen{
-            display: block;
-        }
-        details{
-        display: none;
-        }
-	}
-
-    svg{
-        width: 20px;
-    }
-
-    details .dropdown svg {
-		fill: var(--color-primary);
-	}
-
-    details{
-        margin: 10px;
-        margin-top: 30px;
-        text-align: right;
-    }
-  
-  
-    ul{
-        list-style-type: none;
-        padding-top: 10px;
-    }
-    
-    li{
-        text-decoration: none;
-        padding: 5px;
-        border-radius: 10px;
-    }
-
-    li:hover{
+        .nav-link:hover{
         transform: scale(1.1);
-        transition-duration: 1s;
+		transition-duration: 1s;
         background-color: yellow;
+        border-radius: 20px;
+        padding: 10px;
     }
+	}
 
-    a{
-        text-decoration: none;
-        color: black;
-    }
-
-	details > summary {
-  list-style: none;
-}
-
-details summary::-webkit-details-marker {
-  display:none;
-}
 
 
 
